05fa195ea8b2d1fe0992509cf68ad8c0be5270c5
Rimfoer <polegoshkosergey@mail.ru>
2022-12-12 21:09:08 +0700

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,58 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var flashvars = 
+{
+	//custom flash vars go here as JavaScript object properites
+	//use native data types where necessary i.e. Boolean, Number, String
+};
+var params = 
+{
+	// properties of the embed and object html tags
+	menu: 	"true",
+	scale: 	"noscale", 
+	wmode: 	"transparent",
+	bgcolor: "#000000"
+};
+var attributes = 
+{
+	// any custom HTML attributes
+	id: 	"flashMovie",
+	name: 	"flashMovie"
+};
+
+// does the actual embed, replace all values wrapped in <>
+swfobject.embedSWF("flash/MainSite.swf", "flashContent", "100%", "1210", "9.0.0", "js/swfobject/expressInstall.swf", flashvars, params, attributes);
+
+
+}
+/*
+     FILE ARCHIVED ON 11:10:24 Aug 14, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:42:24 Dec 12, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 8480.182
+  exclusion.robots: 0.092
+  exclusion.robots.policy: 0.083
+  cdx.remote: 0.063
+  esindex: 0.007
+  LoadShardBlock: 76.687 (3)
+  PetaboxLoader3.datanode: 84.852 (4)
+  CDXLines.iter: 20.143 (3)
+  load_resource: 48.54
+  PetaboxLoader3.resolve: 30.663
+*/
