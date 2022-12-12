bebc34e1a66438bf7478f06d756cfae1f76e2fa0
Rimfoer <polegoshkosergey@mail.ru>
2022-12-12 21:18:14 +0700

    Update master.js

--- js/master.js
+++ js/master.js
@@ -31,28 +31,7 @@ var attributes =
 };
 
 // does the actual embed, replace all values wrapped in <>
-swfobject.embedSWF("flash/MainSite.swf", "flashContent", "100%", "1210", "9.0.0", "js/swfobject/expressInstall.swf", flashvars, params, attributes);
+swfobject.embedSWF("armacham/flash/MainSite.swf", "flashContent", "100%", "1210", "9.0.0", "armacham/js/swfobject/expressInstall.swf", flashvars, params, attributes);
 
 
 }
-/*
-     FILE ARCHIVED ON 11:10:24 Aug 14, 2013 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 13:42:24 Dec 12, 2022.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 8480.182
-  exclusion.robots: 0.092
-  exclusion.robots.policy: 0.083
-  cdx.remote: 0.063
-  esindex: 0.007
-  LoadShardBlock: 76.687 (3)
-  PetaboxLoader3.datanode: 84.852 (4)
-  CDXLines.iter: 20.143 (3)
-  load_resource: 48.54
-  PetaboxLoader3.resolve: 30.663
-*/
